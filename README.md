fdef0a59db3af40ed23a98792643a8cd6d53e9d5
Ede Rojin DELİBAŞ <ederojind@gmail.com>
2024-10-07 15:37:05 +0300

    New changes in this repository.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python
+ All Codes, Examples and Projects Using Python
---
+++ TinyApplications/MyFirstModule.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 07:58:03 2024
+Creating My Own Module
+@author: Ede Rojin DELİBAŞ
+"""
+'''
+This module has some values, arrays,a dictionary ,a class and a function
+'''
+print("Thanks for using my module")
+numbers=[22,2016,4,26,2]
+
+family_members={
+    "father":"Jacob",
+    "mother":"wisy",
+    "sister":"cat",
+    "brother":"smokey"
+    }
+
+def Members(array):
+    '''It is shows the element of the population'''
+    print(f'Family members :{family_members}')
+    
+class Person:
+    def speak (self):
+        print("I am speaking right now and It is your turn")
+    
+ 
